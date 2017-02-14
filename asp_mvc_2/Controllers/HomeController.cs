3f9ac5d3029fffaf824bc079ec1664f2d7ff8209
ichanafisah <ichanfs524@gmail.com>
2017-02-14 10:29:33 +0700

    modul_2

--- asp_mvc_2/Controllers/HomeController.cs
+++ asp_mvc_2/Controllers/HomeController.cs
@@ -8,7 +8,7 @@ namespace Asp_mvc_2.Controllers
         public ActionResult Index() {
             return View();
         } 
-
+         
         [Authorize]
         public ActionResult Welcome()
         {
